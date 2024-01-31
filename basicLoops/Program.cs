9685e5c6632d4b885aee541b3070ae90ae3c093e
Jered O'Grady <jered9715@gmail.com>
2024-01-30 23:37:22 -0500

    second commit

--- basicLoops/Program.cs
+++ basicLoops/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Do while loop problem
+bool wantsToPlayAgain = true;
+
+do
+{
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Would you like to continue (y/n)?");
+
+    Console.Write("<<");
+    string userInput = Console.ReadLine();
+    if (userInput.ToLower().Trim() == "n")
+    {
+        wantsToPlayAgain = false;
+        Console.WriteLine("Goodbye!");
+    }
+
+
+}
+while (wantsToPlayAgain == true);
+
+/*
+//For loop problem
+wantsToPlayAgain = true;
+int userNumber = 0;
+
+//do
+
+    Console.Write(">>Enter a number: <<");
+string userEnter = "";
+userEnter = Console.ReadLine();
+userNumber = int.Parse(userEnter);
+    int previousNumber = userNumber;
+
+    for (int i = userNumber; i == 0; i--)
+    {
+        Console.Write(userNumber);
+        userNumber--;
+    }
+
+    Console.WriteLine("");
+
+    for (int i = userNumber; i == previousNumber; i++)
+    {
+        Console.Write(userNumber);
+        userNumber++;
+    }
+*/
+
+Console.Write("Enter a number:");
+int userNumber = int.Parse(Console.ReadLine());
+for (int i = userNumber; i == 0 ; i--) 
+{
+    
+    Console.WriteLine( i );
+}
+
+
+
+//while problem
+int doorCombination = 13579;
+bool doorLocked = true;
+int wrongInput = 0;
+
+while (doorLocked == true)
+{
+    Console.Write("Enter key code >>");
+    int comboInput = int.Parse(Console.ReadLine());
+    if (comboInput == doorCombination)
+    {
+        Console.WriteLine("Welcome Home!");
+        doorLocked = false;
+    }
+    else 
+    {
+    wrongInput++;
+    }
+
+    if (wrongInput == 5)
+    {
+        Console.WriteLine("Too many incorrect attempts. Come back later.");
+        doorLocked = false;
+    }
+
+}
